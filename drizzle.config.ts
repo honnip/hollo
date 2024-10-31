bc9ae7a7074f53aa4e312435823614520dad5976
Honnip <me@honnip.page>
2024-10-31 18:35:53 +0900

    bandaid

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,20 @@
 import type { Config } from "drizzle-kit";
+import pgConnectionString from "pg-connection-string";
 
 // biome-ignore lint/complexity/useLiteralKeys: tsc rants about this (TS4111)
 const databaseUrl = process.env["DATABASE_URL"];
 if (databaseUrl == null) throw new Error("DATABASE_URL must be defined");
+const connectionOptions = pgConnectionString.parse(databaseUrl);
 
 export default {
   schema: "./src/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: databaseUrl,
+    host: connectionOptions.host ?? "localhost",
+    port: connectionOptions.port ? parseInt(connectionOptions.port) : undefined,
+    user: connectionOptions.user,
+    password: connectionOptions.password,
+    database: connectionOptions.database ?? "hollo",
   },
 } satisfies Config;
